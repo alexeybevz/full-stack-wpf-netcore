027305db2ad144f4ee75485964aa4d8ae3de87c0
alexeybevz <bevz.alexei@yandex.ru>
2021-09-03 00:19:10 +0500

    video #1
    Ссылка на туториал: https://www.youtube.com/watch?v=V9UdD96iTbk&list=PLA8ZIAm2I03jSfo18F7Y65XusYzDusYu5&index=1

---
+++ SimpleTrader/SimpleTrader.Domain/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleTrader.Domain.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public User AccountHolder { get; set; }
+        public decimal Balance { get; set; }
+        public IEnumerable<AssetTransaction> AssetTransactions { get; set; }
+    }
+}
---
+++ SimpleTrader/SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DEV-PC;Initial Catalog=SimpleTraderDB;User Id=sa;Password=1;");
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
